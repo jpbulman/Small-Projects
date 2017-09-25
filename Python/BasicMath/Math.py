5141315730f8929b35627801638754b9c95e0101
jpthesolver2 <jpbulman@wpi.edu>
2017-09-25 14:23:26 -0400

    Python

---
+++ Python/BasicMath/Math.py
@@ -0,0 +1,23 @@
+class Math:
+ x = 1
+ a,b =3,4
+
+ mylist = [3,2,3]
+ mylist.append(4)
+
+ print(mylist[0])
+
+ print(mylist[3])
+
+ remainder = 11%6 #5
+
+ print(2**4) #16
+
+ manyhellos = (10 * ("hello!" + " "))
+
+ print(manyhellos)
+
+ if x == 1:
+     print(x)
+
+
